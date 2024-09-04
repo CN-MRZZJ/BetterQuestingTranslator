005969bdc72bd35f1470c6f063d2c367c528ebe9
CN-MRZZJ <zhuzhujun0718@outlook.com>
2024-09-04 13:14:27 +0800

    重构屎山，修改gitignore

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ except Exception as e:
 try:
     # 尝试打开和读取文件
     with open(config['file']['input'], 'r') as file:
-        data = json.load(file)
-        print("成功解析到", len(data["questDatabase:9"]), "个任务")
+        raw_quests = json.load(file)
+        print("成功解析到", len(raw_quests["questDatabase:9"]), "个任务")
 
 # 如果文件不存在或无法以JSON格式加载，则捕获异常并打印错误信息
 except FileNotFoundError:
@@ -26,24 +26,27 @@ except json.JSONDecodeError:
     print(f"错误: 文件 {config['file']['input']} 的内容不是有效的 JSON 格式")
 except Exception as e:
     print(f"一个未知的错误发生：{str(e)}")
-count = 1
-new_data = data
-for key in data["questDatabase:9"].keys():
-    name = data["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["name:8"].strip()
-    desc = data["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["desc:8"].strip()
-    print("————————当前为第", count, "个任务————————")
-    result_1 = driver.translate(name, config['api']['key'], config['api']['secret'],config['api']['url'])
-    new_data["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["name:8"] = result_1
-    print("标题:", name, "->", result_1)
-    time.sleep(config['api']['delay'] / 1000)
-    result_2 = driver.translate(desc, config['api']['key'], config['api']['secret'],config['api']['url'])
-    print("描述:", desc, "->", result_2)
-    new_data["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["desc:8"] = result_2
-    time.sleep(config['api']['delay'] / 1000)
-    print("————————第", count, "个任务已完成————————")
-    count = count + 1
+
+def delay():
+    time.sleep(float(config['api']['delay']) / 1000)
+
+def translate(text):
+    delay()
+    return str(driver.translate(text, config['api']['key'], config['api']['secret'],config['api']['url']))
+
+translated_quests = raw_quests
+
+for index, key in enumerate(raw_quests["questDatabase:9"].keys()):
+    print(f"————————当前为第{index+1}个任务————————")
+    title = raw_quests["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["name:8"].strip()
+    desc = raw_quests["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["desc:8"].strip()
+    translated_quests["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["name:8"] = translate(title)
+    print(f"标题:{title}->{translated_quests["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["name:8"]}")
+    translated_quests["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["desc:8"] = translate(desc)
+    print(f"描述:{desc}->{translated_quests["questDatabase:9"][key]["properties:10"]["betterquesting:10"]["desc:8"]}")
+    print(f"————————第{index+1}个任务已完成————————")
 
 with open(config["file"]['output'], 'w', encoding='utf-8') as f:
-    json.dump(new_data, f)
+    json.dump(translated_quests, f)
 
 print("————————执行完成————————")
